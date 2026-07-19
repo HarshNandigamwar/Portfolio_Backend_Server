bbaaded8c564d7ca7ae5fb4897e31530b6581e42
Shriharsh Nandigamwar <nandigamwarharsh@gmail.com>
2026-07-19 16:45:45 +0530

    add course model

---
+++ src/models/course.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const courseSchema = new mongoose.Schema(
+  {
+    courseName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    courseDescription: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    verifyCredentialLink: {
+      type: String,
+      trim: true,
+    },
+    keySkills: {
+      type: [String],
+      required: true,
+    },
+    certificateImage: {
+      type: String, // Cloudinary URL
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export const Course = mongoose.model("Course", courseSchema);
